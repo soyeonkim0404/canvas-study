b9828d2f1254ed4837bace601473c6a23bdc152c
kimsoyeon <soyeon.kim1@emotion.co.kr>
2023-06-05 15:39:21 +0900

    Initial commit

---
+++ canvas-fireWork/js/Spark.js
@@ -0,0 +1,30 @@
+import CanvasOption from './CanvasOption.js'
+import { randomNumBetween } from './utils.js'
+
+export default class Spark extends CanvasOption {
+    constructor(x, y, vx, vy, opacity, colorDeg) {
+        super()
+        this.x = x
+        this.y = y
+        this.vx = vx
+        this.vy = vy
+        this.opacity = opacity
+        this.colorDeg = colorDeg
+    }
+
+    update() {
+        this.opacity -= 0.015
+
+        this.x += this.vx
+        this.x -= 0
+        this.y += this.vy
+    }
+
+    draw() {
+        this.ctx.beginPath()
+        this.ctx.arc(this.x, this.y, 1, 0, Math.PI * 2)
+        this.ctx.fillStyle = `hsla(${this.colorDeg}, 100%, 65%, ${this.opacity})`
+        this.ctx.fill()
+        this.ctx.closePath()
+    }
+}
---
+++ canvas-fireWork/js/utils.js
@@ -0,0 +1,7 @@
+export function randomNumBetween(min, max) {
+    return Math.random() * (max - min) + min
+}
+
+export function hypotenuse(x,y){
+    return Math.sqrt(Math.pow(x,2) + Math.pow(y, 2))
+}
